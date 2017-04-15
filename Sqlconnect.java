cab5ac2ded70ec5657fd1b530310e3b103b4b801
TanyiFrank <tt222fi@student.lnu.se>
2017-04-15 20:09:55 +0200

    database connection class

---
+++ Sqlconnect.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Sqlconnect {
+
+	
+
+	public static Connection DbConnector() throws SQLException {
+
+		try {
+			Connection con = null;
+			
+		Class.forName("org.sqlite.JDBC");
+
+			con = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+			return con;
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+}
