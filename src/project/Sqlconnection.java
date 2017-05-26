374f2f7c614ba34f08a32c167384838fd0200cc2
andreas1995 <suidos999@gmail.com>
2017-05-26 20:44:53 +0200

    Update Sqlconnection.java

--- src/project/Sqlconnection.java
+++ src/project/Sqlconnection.java
@@ -1,8 +1,11 @@
 package project;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Sqlconnection {
 
@@ -13,13 +16,156 @@ public static Connection DbConnector() throws SQLException {
 		try {
 			Connection con = null;
 			
-		Class.forName("org.sqlite.JDBC");
+		     Class.forName("org.sqlite.JDBC");
 
 			con = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+			
+			if(!tableExist(con,"TimeLine"))
+				TimelineCreator();
+			
+			if(!tableExist(con,"SaveEvent"))
+				SaveEventCreator();
+			
 			return con;
 		} catch (ClassNotFoundException | SQLException e) {
 			System.out.println(e);
 		}
+		
 		return null;
 	}
+
+
+    public static boolean tableExist(Connection conn, String tableName) throws SQLException {
+    boolean tExists = false;
+    try (ResultSet rs = conn.getMetaData().getTables(null, null, tableName, null)) {
+        while (rs.next()) { 
+            String tName = rs.getString("TABLE_NAME");
+            if (tName != null && tName.equals(tableName)) {
+                tExists = true;
+                break;
+            }
+        }
+    }
+    return tExists;
+}
+  
+	public static  void SaveEventCreator(){
+		   Connection conn = null;
+		   Statement stmt = null;
+		   
+		   
+		   
+		   try{
+		      
+			   Class.forName("org.sqlite.JDBC");
+
+		      
+		      System.out.println("Connecting to a selected database...");
+		  	conn = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+		      
+		      
+		      System.out.println("Connected database successfully...");
+		      
+		      
+		    
+		      System.out.println("Creating table in given database...");
+		      stmt = conn.createStatement();
+		      
+		      String sql = "CREATE TABLE SaveEvent " +
+		                   "(ID INTEGER , " +
+		                   " Name TEXT, " + 
+		                   " Description TEXT, " +  
+		                   " StartDate TEXT, " +
+		                   " EndDate TEXT, " +
+		                   " Image BLOB)";
+		      stmt.executeUpdate(sql);
+		      
+		    
+		      System.out.println("Created table in given database...");
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }// do nothing
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   System.out.println("Goodbye!");
+		}//end main
+		
+	   public static  void TimelineCreator(){
+		   Connection conn = null;
+		   Statement stmt = null;
+		   
+		    
+		   
+		   try{
+		      //STEP 2: Register JDBC driver
+			   Class.forName("org.sqlite.JDBC");
+
+		      //STEP 3: Open a connection
+		      System.out.println("Connecting to a selected database...");
+		  	conn = DriverManager.getConnection("jdbc:sqlite:database.sqlite");
+		      
+		      
+		      System.out.println("Connected database successfully...");
+		      
+//		      "INSERT INTO TimeLine (Title, Description, StartDate, EndDate) VALUES (?,?,?,?)  ";
+		            
+		      
+		      //STEP 4: Execute a query
+		      System.out.println("Creating table in given database...");
+		      stmt = conn.createStatement();
+		      
+		      String sql = "CREATE TABLE TimeLine " +
+		                   "(Title TEXT, " + 
+		                   " Description TEXT, " +  
+		                   " StartDate TEXT, " +
+		                   " EndDate TEXT) ";
+		                   
+		      stmt.executeUpdate(sql);
+		      
+		      
+		      
+		      System.out.println("Created table in given database...");
+		   }catch(SQLException se){
+		      //Handle errors for JDBC
+		      se.printStackTrace();
+		   }catch(Exception e){
+		      //Handle errors for Class.forName
+		      e.printStackTrace();
+		   }finally{
+		      //finally block used to close resources
+		      try{
+		         if(stmt!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		      }// do nothing
+		      try{
+		         if(conn!=null)
+		            conn.close();
+		      }catch(SQLException se){
+		         se.printStackTrace();
+		      }//end finally try
+		   }//end try
+		   System.out.println("Goodbye!");
+		}//end main
+		//end JDBCExample
+		
+		
+
+
+
 }
